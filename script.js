f0fbc893fcbdd709b5152b022a38876dc9aeab1c
Sandesh Jung Kunwar <sandeshjung@gmail.com>
2024-02-01 16:55:18 +0545

    feat: add obstacle

--- script.js
+++ script.js
@@ -10,12 +10,20 @@ let bugHeight = 38;
 let bugX;
 let bugY = (6 * boardHeight) / 8;
 
+// wires
 let wires = [
     { x: boardWidth / 5 },
     { x: (2 * boardWidth) / 5 },
     { x: (3 * boardWidth) / 5 },
     { x: (4 * boardWidth) / 5 }
 ];
+
+// obstacles
+let obstacleWidth = 60;
+let obstacleHeight = 60;
+let obstacleSpeed = 3;
+let obstacles = [];
+
 
 window.onload = function () {
     board = document.getElementById("board");
@@ -26,8 +34,13 @@ window.onload = function () {
     // Initialize bug position to the middle wire
     bugX = wires[Math.floor(wires.length / 2)].x;
 
+    // New obstacle 
+    setInterval(function () {
+        obstacles.push(createNewObstacle());
+    }, 1000);
+
     // Draw initial state
-    draw();
+    update();
 
     window.addEventListener("keydown", function (event) {
         switch (event.key) {
@@ -51,12 +64,43 @@ function moveBug(direction) {
 
     // Update bug position based on the selected wire
     bugX = wires[wireIndex].x;
-
-    // Redraw the canvas
-    draw();
 }
 
-function draw() {
+// function to  create a new obstacle 
+function createNewObstacle() {
+    let randomWireIndex = Math.floor(Math.random() * wires.length);
+    let obstacleX = wires[randomWireIndex].x;
+    let obstacleY = 0 - obstacleHeight; 
+    return {x: obstacleX, y: obstacleY}
+}
+
+
+// function to update obstacle position
+function updateObstacles() {
+    for (let obstacle of obstacles) {
+        obstacle.y += obstacleSpeed;
+
+        if (obstacle.y > boardHeight) {
+            obstacles.splice(obstacles.indexOf(obstacle), 1);
+        }
+    }
+}
+
+
+// function to draw obstacles
+function drawObstacles() {
+    context.fillStyle = "red";
+    for (let obstacle of obstacles) {
+        context.fillRect(
+            obstacle.x - obstacleWidth / 2,
+            obstacle.y - obstacleHeight / 2,
+            obstacleWidth,
+            obstacleHeight
+        )
+    }
+}
+
+function update() {
     context.clearRect(0, 0, board.width, board.height);
 
     // Draw wires
@@ -65,7 +109,13 @@ function draw() {
         context.fillRect(wire.x - 1.5, 0, 3, board.height);
     }
 
+    updateObstacles();
+    drawObstacles();
+
     // Draw bug
     context.fillStyle = "green";
     context.fillRect(bugX - (bugWidth / 2), bugY - (bugHeight / 2), bugWidth, bugHeight);
+
+    requestAnimationFrame(update);
+
 }
